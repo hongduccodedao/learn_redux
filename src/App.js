93d24c16d23c2484b714c6a4816f9584f53ffe1b
hongduccodedao <contact.hongduc@gmail.com>
2023-04-17 09:04:41 +0700

    🚀 chore(App.js): add dark mode support 🚀 feat(App.js): add Card component and toggle dark mode button 🚀 feat(globalSlice.js): add global slice for dark mode state 🚀 feat(useDarkMode.js): add hook for dark mode state and local storage 🚀 feat(useLocalStorage.js): add hook for local storage 🚀 feat(useMedia.js): add hook for media queries 🚀 chore(configureStore.js): add global slice to store 🚀 feat(Card.js): add Card component UI
    The changes add support for dark mode to the application. A new Card component is added to the App.js file, and a toggle button is added to switch between light and dark mode. The globalSlice.js file is added to manage the dark mode state, and the useDarkMode.js, useLocalStorage.js, and useMedia.js hooks are added to manage the dark mode state and local storage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from "react";
-import Counter from "./components/Counter";
-import { useSelector } from "react-redux";
+// import Counter from "./components/Counter";
+import { useDispatch, useSelector } from "react-redux";
+import Card from "./components/Card";
+import useDarkMode from "./hooks/useDarkMode";
+import { toggleDarkMode } from "./redux-toolkit/globalSlice";
 
 const App = () => {
-  const { count } = useSelector((state) => state.counter);
+  // const { count } = useSelector((state) => state.counter);
+  const global = useSelector((state) => state.global);
+  console.log("🚀 ~ App ~ global:", global);
+
+  const dispatch = useDispatch();
+
+  const [darkMode, setDarkMode] = useDarkMode();
+
+  const handleChangeMode = () => {
+    setDarkMode(!darkMode);
+    dispatch(toggleDarkMode(!darkMode));
+  };
 
   return (
     <div className="">
-      <h2>
+      {/* <h2>
         The count from App: <span className="text-blue-500">{count}</span>
       </h2>
-      <Counter />
+      <Counter /> */}
+      <Card />
+      <button onClick={handleChangeMode}>Toogle dark mode</button>
     </div>
   );
 };
